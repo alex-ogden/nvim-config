b42eb69e537531e6b3928673af2498a38154634c
Alex Ogden <alex.s.ogden@icloud.com>
2025-11-11 12:51:04 +0000

    changes

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -9,8 +9,8 @@ opt.number = true
 opt.relativenumber = true
 
 -- Tabs & indentation
-opt.tabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 opt.smartindent = true
