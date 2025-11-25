4b67dfbe69fc949d3a3cf925bf463b59f8077eeb
Alex Ogden <alex.s.ogden@icloud.com>
2025-11-25 13:14:18 +0000

    colourscheme changes, again

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,38 +1,35 @@
 return {
-	-- {
-	-- 	"scottmckendry/cyberdream.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("cyberdream").setup({
-	-- 			transparent = false,
-	-- 			italic_comments = true,
-	-- 			hide_fillchars = false,
-	-- 			borderless_telescope = true,
-	-- 			terminal_colors = true,
-	-- 		})
-	-- 		vim.cmd("colo cyberdream")
-	-- 	end,
-	-- },
-
-	-- {
-	--   "eddyekofo94/gruvbox-flat.nvim",
-	--   lazy = false,
-	--   priority = 1000,
-	--   config = function()
-	--     vim.cmd("colorscheme gruvbox-flat")
-	--   end,
-	-- },
-
 	{
-		"samharju/synthweave.nvim",
+		"scottmckendry/cyberdream.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd("colorscheme synthweave")
+			require("cyberdream").setup({
+				transparent = false,
+				italic_comments = true,
+				hide_fillchars = false,
+				borderless_telescope = true,
+				terminal_colors = true,
+			})
+			vim.cmd("colo cyberdream")
 		end,
 	},
-
+	-- {
+	-- 	"eddyekofo94/gruvbox-flat.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.cmd("colorscheme gruvbox-flat")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"samharju/synthweave.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.cmd("colorscheme synthweave")
+	-- 	end,
+	-- },
 	-- {
 	-- 	"ray-x/starry.nvim",
 	-- 	lazy = false,
