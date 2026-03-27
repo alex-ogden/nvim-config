c3ce521015dbcfafcbc3b5fd7937a597c391498c
Alex <alex.s.ogden@icloud.com>
2026-03-27 10:14:16 +0000

    Updating more stuff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,208 +1,177 @@
 return {
-	-- Mason: LSP installer
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
+    -- Mason: LSP installer
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            })
+        end,
+    },
 
-	-- Mason LSP Config
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"clangd", -- C/C++
-					"rust_analyzer", -- Rust
-					"pyright", -- Python
-					"zls", -- Zig
-					"lua_ls", -- Lua
-					"bashls", -- Bash
-					"terraformls", -- Terraform
-					"jsonls", -- JSON
-					"yamlls", -- YAML
-					"marksman", -- Markdown
-				},
-				automatic_installation = true,
-			})
-		end,
-	},
+    -- Mason LSP Config
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "clangd",       -- C/C++
+                    "rust_analyzer",-- Rust
+                    "pyright",      -- Python
+                    "zls",          -- Zig
+                    "lua_ls",       -- Lua
+                    "bashls",       -- Bash
+                    "terraformls",  -- Terraform
+                    "jsonls",       -- JSON
+                    "yamlls",       -- YAML
+                    "marksman",     -- Markdown
+                    "gopls",        -- Go language server
+                },
+                automatic_installation = true,
+            })
+        end,
+    },
 
-	-- LSP Config
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"williamboman/mason-lspconfig.nvim",
-		},
-		config = function()
-			local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    -- LSP Config
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "williamboman/mason-lspconfig.nvim",
+        },
+        config = function()
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-			-- LSP keybindings
-			local on_attach = function(client, bufnr)
-				local opts = { buffer = bufnr, silent = true }
+            local on_attach = function(client, bufnr)
+                local opts = { buffer = bufnr, silent = true }
 
-				opts.desc = "Show LSP references"
-				vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+                -- LSP keybindings
+                vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+                vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+                vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+                vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+                vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+                vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+            end
 
-				opts.desc = "Go to declaration"
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+            local capabilities = cmp_nvim_lsp.default_capabilities()
 
-				opts.desc = "Show LSP definitions"
-				vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+            vim.diagnostic.config({
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = " ",
+                        [vim.diagnostic.severity.WARN] = " ",
+                        [vim.diagnostic.severity.HINT] = "󰠠 ",
+                        [vim.diagnostic.severity.INFO] = " ",
+                    },
+                },
+            })
 
-				opts.desc = "Show LSP implementations"
-				vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+            local default_config = {
+                capabilities = capabilities,
+                on_attach = on_attach,
+            }
 
-				opts.desc = "Show LSP type definitions"
-				vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+            -- Go
+            vim.lsp.config("gopls", vim.tbl_extend("force", default_config, {
+                settings = {
+                    gopls = {
+                        analyses = {
+                            unusedparams = true,
+                            shadow = true,
+                        },
+                        staticcheck = true,
+                    },
+                },
+            }))
+            vim.lsp.enable("gopls")
 
-				opts.desc = "See available code actions"
-				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+            -- C/C++
+            vim.lsp.config("clangd", vim.tbl_extend("force", default_config, {
+                cmd = {
+                    "clangd",
+                    "--background-index",
+                    "--clang-tidy",
+                    "--header-insertion=iwyu",
+                    "--completion-style=detailed",
+                },
+            }))
+            vim.lsp.enable("clangd")
 
-				opts.desc = "Smart rename"
-				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+            -- Rust
+            vim.lsp.config("rust_analyzer", vim.tbl_extend("force", default_config, {
+                settings = {
+                    ["rust-analyzer"] = {
+                        checkOnSave = { command = "clippy" },
+                    },
+                },
+            }))
+            vim.lsp.enable("rust_analyzer")
 
-				opts.desc = "Show buffer diagnostics"
-				vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+            -- Python
+            vim.lsp.config("pyright", vim.tbl_extend("force", default_config, {
+                settings = { python = { analysis = { typeCheckingMode = "basic" } } },
+            }))
+            vim.lsp.enable("pyright")
 
-				opts.desc = "Show line diagnostics"
-				vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+            -- Zig
+            vim.lsp.config("zls", default_config)
+            vim.lsp.enable("zls")
 
-				opts.desc = "Go to previous diagnostic"
-				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+            -- Bash
+            vim.lsp.config("bashls", default_config)
+            vim.lsp.enable("bashls")
 
-				opts.desc = "Go to next diagnostic"
-				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+            -- Terraform
+            vim.lsp.config("terraformls", default_config)
+            vim.lsp.enable("terraformls")
 
-				opts.desc = "Show documentation for what is under cursor"
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+            -- JSON
+            vim.lsp.config("jsonls", default_config)
+            vim.lsp.enable("jsonls")
 
-				opts.desc = "Restart LSP"
-				vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
-			end
+            -- YAML
+            vim.lsp.config("yamlls", vim.tbl_extend("force", default_config, {
+                settings = {
+                    yaml = { schemas = { ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*" } },
+                },
+            }))
+            vim.lsp.enable("yamlls")
 
-			-- Capabilities for autocompletion
-			local capabilities = cmp_nvim_lsp.default_capabilities()
+            -- Markdown
+            vim.lsp.config("marksman", default_config)
+            vim.lsp.enable("marksman")
 
-			-- Diagnostic signs (Neovim 0.11+ API)
-			vim.diagnostic.config({
-				signs = {
-					text = {
-						[vim.diagnostic.severity.ERROR] = " ",
-						[vim.diagnostic.severity.WARN] = " ",
-						[vim.diagnostic.severity.HINT] = "󰠠 ",
-						[vim.diagnostic.severity.INFO] = " ",
-					},
-				},
-			})
-
-			-- Default config for all servers
-			local default_config = {
-				capabilities = capabilities,
-				on_attach = on_attach,
-			}
-
-			-- Configure LSP servers using new Neovim 0.11 API
-
-			-- C/C++
-			vim.lsp.config("clangd", vim.tbl_extend("force", default_config, {
-				cmd = {
-					"clangd",
-					"--background-index",
-					"--clang-tidy",
-					"--header-insertion=iwyu",
-					"--completion-style=detailed",
-				},
-			}))
-
-			-- Rust
-			vim.lsp.config("rust_analyzer", vim.tbl_extend("force", default_config, {
-				settings = {
-					["rust-analyzer"] = {
-						checkOnSave = {
-							command = "clippy",
-						},
-					},
-				},
-			}))
-
-			-- Python
-			vim.lsp.config("pyright", vim.tbl_extend("force", default_config, {
-				settings = {
-					python = {
-						analysis = {
-							typeCheckingMode = "basic",
-						},
-					},
-				},
-			}))
-
-			-- Zig
-			vim.lsp.config("zls", default_config)
-
-			-- Bash
-			vim.lsp.config("bashls", default_config)
-
-			-- Terraform
-			vim.lsp.config("terraformls", default_config)
-
-			-- JSON
-			vim.lsp.config("jsonls", default_config)
-
-			-- YAML
-			vim.lsp.config("yamlls", vim.tbl_extend("force", default_config, {
-				settings = {
-					yaml = {
-						schemas = {
-							["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-						},
-					},
-				},
-			}))
-
-			-- Markdown
-			vim.lsp.config("marksman", default_config)
-
-			-- Lua (for Neovim config)
-			vim.lsp.config("lua_ls", vim.tbl_extend("force", default_config, {
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-						workspace = {
-							library = {
-								[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-								[vim.fn.stdpath("config") .. "/lua"] = true,
-							},
-						},
-					},
-				},
-			}))
-
-			-- Enable LSP servers
-			vim.lsp.enable("clangd")
-			vim.lsp.enable("rust_analyzer")
-			vim.lsp.enable("pyright")
-			vim.lsp.enable("zls")
-			vim.lsp.enable("bashls")
-			vim.lsp.enable("terraformls")
-			vim.lsp.enable("jsonls")
-			vim.lsp.enable("yamlls")
-			vim.lsp.enable("marksman")
-			vim.lsp.enable("lua_ls")
-		end,
-	},
+            -- Lua
+            vim.lsp.config("lua_ls", vim.tbl_extend("force", default_config, {
+                settings = {
+                    Lua = {
+                        diagnostics = { globals = { "vim" } },
+                        workspace = {
+                            library = {
+                                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                                [vim.fn.stdpath("config") .. "/lua"] = true,
+                            },
+                        },
+                    },
+                },
+            }))
+            vim.lsp.enable("lua_ls")
+        end,
+    },
 }
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -8,14 +8,58 @@ return {
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
     "onsails/lspkind.nvim",     -- vs-code like pictograms
+    "hrsh7th/cmp-nvim-lsp",     -- LSP completion source
   },
   config = function()
     local cmp = require("cmp")
     local luasnip = require("luasnip")
     local lspkind = require("lspkind")
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
 
     -- Load friendly-snippets
     require("luasnip.loaders.from_vscode").lazy_load()
+
+    -- Define Go snippets
+    luasnip.add_snippets("go", {
+      -- func snippet
+      luasnip.snippet("func", {
+        luasnip.text_node("func "), 
+        luasnip.insert_node(1, "name"),
+        luasnip.text_node("("),
+        luasnip.insert_node(2),
+        luasnip.text_node(") "),
+        luasnip.insert_node(3),
+        luasnip.text_node({" {", "\t"}),
+        luasnip.insert_node(0),
+        luasnip.text_node({"", "}"}),
+      }),
+      -- if snippet
+      luasnip.snippet("if", {
+        luasnip.text_node("if "),
+        luasnip.insert_node(1, "condition"),
+        luasnip.text_node({" {", "\t"}),
+        luasnip.insert_node(0),
+        luasnip.text_node({"", "}"}),
+      }),
+      -- for snippet
+      luasnip.snippet("for", {
+        luasnip.text_node("for "),
+        luasnip.insert_node(1, "i := 0; i < n; i++"),
+        luasnip.text_node({" {", "\t"}),
+        luasnip.insert_node(0),
+        luasnip.text_node({"", "}"}),
+      }),
+      -- fmt.Println snippet
+      luasnip.snippet("print", {
+        luasnip.text_node('fmt.Println("'),
+        luasnip.insert_node(1),
+        luasnip.text_node('")'),
+      }),
+    })
+
+    -- Ensure LSP capabilities are enhanced for nvim-cmp
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
     cmp.setup({
       completion = {
@@ -27,28 +71,55 @@ return {
         end,
       },
       mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       }),
-      -- sources for autocompletion
       sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" },  -- text within current buffer
-        { name = "path" },    -- file system paths
+        { name = "nvim_lsp" }, -- LSP completions (gopls)
+        { name = "luasnip" },  -- snippets
+        { name = "buffer" },
+        { name = "path" },
       }),
-      -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format({
           maxwidth = 50,
           ellipsis_char = "...",
         }),
       },
+    })
+
+    -- Trigger completion automatically for Go
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "go",
+      callback = function()
+        cmp.setup.buffer({
+          sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "luasnip" },
+            { name = "buffer" },
+            { name = "path" },
+          }),
+        })
+      end,
     })
   end,
 }
